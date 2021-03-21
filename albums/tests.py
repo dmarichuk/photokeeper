19d0574d8065fcd6cc138bc92bd0ad2102cf580e
Danila <d_marichuk@mail.ru>
2021-03-21 08:39:04 +0300

    added dist

--- albums/tests.py
+++ albums/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from albums.models import Album
+
+
+User = get_user_model()
+class AlbumTest(TestCase):
+
+    def setUp(self):
+        
+        self.client = Client()
+
+        self.testUser = User.objects.create(username='testUser')
+        self.testAlbum = Album.objects.create(title='testTitle', description='testDescription')
+    
+    def testAlbumCreation(self):
+
+        self.client.force_login(self.testUser)
+        
+        response = self.client.get(
+            reverse('new_album', kwargs={'username': self.testUser.username}))
+        
+        self.assertEqual(response.status_code, 200)   
+        
+        response = self.client.post( 
+            reverse('new_album', kwargs={'username': self.testUser.username}),
+            {'title': 'testTitle', 'description': 'testDescription'})
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.testUser.albums.count(), 1)
+    
+    def testAlbumGet(self):
+
+        self.client.force_login(self.testUser)
+        self.testAlbum.creator = self.testUser
+
+        response = self.client.get(
+            reverse('all_albums', kwargs={'username': self.testUser.username}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['page'].first, self.testAlbum)
+
+
